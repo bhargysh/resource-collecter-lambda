d2022d5112e93b223270f9067af15116e6a44690
Bharg Sharma <bsha492@aucklanduni.ac.nz>
2021-05-26 12:59:17 +1000

    Add input text validation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
-exports.handler = async (event) => {
-    console.log("Event received 👀", event);
+exports.handler = async (event, context, callback) => {
+    const channels = ['C0231DPGQ2W'];
+    const isValidatedChannel = channels.includes(event.event.channel) && event.event.channel_type === 'channel';
+    const urlRegex = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/; //https://regexr.com/39nr7
+    const extractUrl = (text) => {
+        const result = text.match(urlRegex)
+    }
+
+    const containsUrl = (text) => urlRegex.test(text);
+
+    if (isValidatedChannel) {
+        // const maybeResourceLink = extractUrl(event.event.text);
+        if (containsUrl(event.event.text)) {
+            console.log("YASSSS", event.event.text);
+        }
+    }
+
     const response = {
         challenge: event.challenge,
     };
     return response;
 };
+
+/* Logic
+get the text from event body
+if it contains a link (using regex)
+    build a json payload to send to ES
+    asynchronously send data to ES
+else
+    do nothing
+*/
