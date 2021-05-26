b03378d0604ed6ba182ef77cfb38f7a8724a1246
Bharg Sharma <bsha492@aucklanduni.ac.nz>
2021-05-26 13:39:14 +1000

    Build a payload to pass to ES

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ exports.handler = async (event, context, callback) => {
     const channels = ['C0231DPGQ2W'];
     const isValidatedChannel = channels.includes(event.event.channel) && event.event.channel_type === 'channel';
     const urlRegex = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/; //https://regexr.com/39nr7
-    const extractUrl = (text) => {
-        const result = text.match(urlRegex)
-    }
-
     const containsUrl = (text) => urlRegex.test(text);
 
     if (isValidatedChannel) {
-        // const maybeResourceLink = extractUrl(event.event.text);
-        if (containsUrl(event.event.text)) {
-            console.log("YASSSS", event.event.text);
+        const inputText = event.event.text;
+        if (containsUrl(inputText)) {
+            const [title, categories, url] = inputText.split(", ")
+            const hasCategories = categories ? categories : ""
+
+            const esPayload = JSON.stringify({
+                "title": title,
+                "categories": hasCategories.split("/"),
+                "url": url
+            })
+            console.log("ES PAYLOAD", esPayload);
+            // asynchronously send data to ES
+        } else {
+            console.log(`Message with id: ${event.event.client_msg_id} does not contain a url`);
         }
     }
 
@@ -21,11 +28,4 @@ exports.handler = async (event, context, callback) => {
     return response;
 };
 
-/* Logic
-get the text from event body
-if it contains a link (using regex)
-    build a json payload to send to ES
-    asynchronously send data to ES
-else
-    do nothing
-*/
+//TODO: bug when there is no category passed
